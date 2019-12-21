4d1e36e1bcfbe82580a9fde50122d40f2d71c738
Catbender15 <57030465+Catbender15@users.noreply.github.com>
2019-12-21 10:32:35 +0200

    object3 v4

--- src/Object/Object3/TicTacToeAi/TicTacToeAi.java
+++ src/Object/Object3/TicTacToeAi/TicTacToeAi.java
@@ -1,7 +1,5 @@
 package Object.Object3.TicTacToeAi;
 
-import Object.Object3.TicTacToe;
-
 import java.util.Random;
 
 public class TicTacToeAi {
@@ -13,24 +11,25 @@ public class TicTacToeAi {
     private int xAI;
     private int yAI;
 
-    public void mainAI(){
+    public void mainAI() {
         calculateValue();
-        printValue();
+        //printValue();
         getCor();
     }
-    private void getCor(){
+
+    private void getCor() {
         Random random = new Random();
-        int[] xM = {0,0,0,0,0,0,0,0,0};
-        int[] yM = {0,0,0,0,0,0,0,0,0};
+        int[] xM = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+        int[] yM = {0, 0, 0, 0, 0, 0, 0, 0, 0};
         int maxValue = 0;
         int m = 0;
         for (int i = 0; i <= 2; i++) {
             for (int a = 0; a <= 2; a++) {
-                if(maxValue < value[i][a]){
+                if (maxValue < value[i][a]) {
                     maxValue = value[i][a];
                     m = 0;
                 }
-                if(maxValue == value[i][a]){
+                if (maxValue == value[i][a]) {
                     xM[m] = i;
                     yM[m] = a;
                     m++;
@@ -41,7 +40,8 @@ public class TicTacToeAi {
         xAI = xM[s];
         yAI = yM[s];
     }
-    private static void printValue(){
+
+    private static void printValue() {
         System.out.println("Galimybe laimeti:");
         System.out.println("------------------------------------------------------");
         System.out.println();
@@ -61,7 +61,8 @@ public class TicTacToeAi {
         System.out.println("------------------------------------------------------");
         System.out.println();
     }
-    private static void calculateValue(){
+
+    private static void calculateValue() {
         int win[][] = calculateChanceWin();
         int lose[][] = calculateChanceLose();
         for (int i = 0; i <= 2; i++) {
@@ -69,9 +70,9 @@ public class TicTacToeAi {
                 value[i][a] = win[i][a] + lose[i][a];
             }
         }
-        return;
     }
-    private static int[][] calculateChanceWin(){
+
+    private static int[][] calculateChanceWin() {
         TicTacToeMain ticTacToeMain = new TicTacToeMain();
         String[][] cor = ticTacToeMain.getCor();
 
@@ -82,49 +83,50 @@ public class TicTacToeAi {
         };
         for (int i = 0; i <= 2; i++) {
             for (int a = 0; a <= 2; a++) {
-                if((cor[i][a].equals("X")) || (cor[i][a].equals("0"))){
+                if ((cor[i][a].equals("X")) || (cor[i][a].equals("0"))) {
                     win[i][a] = -1;
-                }else if(i == 0 && a == 0){
-                    win[i][a] = calcWin(0,1,0,2, win[i][a]);
-                    win[i][a] = calcWin(1,0,2,0, win[i][a]);
-                    win[i][a] = calcWin(1,1,2,2, win[i][a]);
-                }else if(i == 0 && a == 1){
-                    win[i][a] = calcWin(0,0,0,2, win[i][a]);
-                    win[i][a] = calcWin(1,1,2,1, win[i][a]);
-                }else if(i == 0 && a == 2){
-                    win[i][a] = calcWin(0,0,0,1, win[i][a]);
-                    win[i][a] = calcWin(1,2,2,2, win[i][a]);
-                    win[i][a] = calcWin(1,1,2,0, win[i][a]);
-                }else if(i == 1 && a == 0){
-                    win[i][a] = calcWin(0,0,2,0, win[i][a]);
-                    win[i][a] = calcWin(1,1,1,2, win[i][a]);
-                }else if(i == 1 && a == 1){ // 4
-                    win[i][a] = calcWin(0,0,2,2, win[i][a]);
-                    win[i][a] = calcWin(0,2,2,0, win[i][a]);
-                    win[i][a] = calcWin(1,0,1,2, win[i][a]);
-                    win[i][a] = calcWin(0,1,2,1, win[i][a]);
-                }else if(i == 1 && a == 2){
-                    win[i][a] = calcWin(0,2,2,2, win[i][a]);
-                    win[i][a] = calcWin(1,0,1,1, win[i][a]);
-                }else if(i == 2 && a == 0){
-                    win[i][a] = calcWin(0,0,1,0, win[i][a]);
-                    win[i][a] = calcWin(0,2,1,1, win[i][a]);
-                    win[i][a] = calcWin(2,1,2,2, win[i][a]);
-                }else if(i == 2 && a == 1){
-                    win[i][a] = calcWin(0,1,1,1, win[i][a]);
-                    win[i][a] = calcWin(2,0,2,2, win[i][a]);
-                }else if(i == 2 && a == 2) {
+                } else if (i == 0 && a == 0) {
+                    win[i][a] = calcWin(0, 1, 0, 2, win[i][a]);
+                    win[i][a] = calcWin(1, 0, 2, 0, win[i][a]);
+                    win[i][a] = calcWin(1, 1, 2, 2, win[i][a]);
+                } else if (i == 0 && a == 1) {
+                    win[i][a] = calcWin(0, 0, 0, 2, win[i][a]);
+                    win[i][a] = calcWin(1, 1, 2, 1, win[i][a]);
+                } else if (i == 0 && a == 2) {
+                    win[i][a] = calcWin(0, 0, 0, 1, win[i][a]);
+                    win[i][a] = calcWin(1, 2, 2, 2, win[i][a]);
+                    win[i][a] = calcWin(1, 1, 2, 0, win[i][a]);
+                } else if (i == 1 && a == 0) {
+                    win[i][a] = calcWin(0, 0, 2, 0, win[i][a]);
+                    win[i][a] = calcWin(1, 1, 1, 2, win[i][a]);
+                } else if (i == 1 && a == 1) { // 4
+                    win[i][a] = calcWin(0, 0, 2, 2, win[i][a]);
+                    win[i][a] = calcWin(0, 2, 2, 0, win[i][a]);
+                    win[i][a] = calcWin(1, 0, 1, 2, win[i][a]);
+                    win[i][a] = calcWin(0, 1, 2, 1, win[i][a]);
+                } else if (i == 1 && a == 2) {
+                    win[i][a] = calcWin(0, 2, 2, 2, win[i][a]);
+                    win[i][a] = calcWin(1, 0, 1, 1, win[i][a]);
+                } else if (i == 2 && a == 0) {
+                    win[i][a] = calcWin(0, 0, 1, 0, win[i][a]);
+                    win[i][a] = calcWin(0, 2, 1, 1, win[i][a]);
+                    win[i][a] = calcWin(2, 1, 2, 2, win[i][a]);
+                } else if (i == 2 && a == 1) {
+                    win[i][a] = calcWin(0, 1, 1, 1, win[i][a]);
+                    win[i][a] = calcWin(2, 0, 2, 2, win[i][a]);
+                } else if (i == 2 && a == 2) {
                     win[i][a] = calcWin(0, 0, 1, 1, win[i][a]);
                     win[i][a] = calcWin(0, 2, 1, 2, win[i][a]);
                     win[i][a] = calcWin(2, 0, 2, 1, win[i][a]);
-                }else {
+                } else {
                     System.out.println("Ivyko klaida AI chance win");
                 }
             }
         }
         return win;
     }
-    private static int[][] calculateChanceLose(){
+
+    private static int[][] calculateChanceLose() {
         TicTacToeMain ticTacToeMain = new TicTacToeMain();
         String[][] cor = ticTacToeMain.getCor();
         int[][] lose = {
@@ -134,42 +136,42 @@ public class TicTacToeAi {
         };
         for (int i = 0; i <= 2; i++) {
             for (int a = 0; a <= 2; a++) {
-                if((cor[i][a].equals("X")) || (cor[i][a].equals("0"))){
+                if ((cor[i][a].equals("X")) || (cor[i][a].equals("0"))) {
                     lose[i][a] = -1;
-                }else if(i == 0 && a == 0){
-                    lose[i][a] = calcLose(0,1,0,2, lose[i][a]);
-                    lose[i][a] = calcLose(1,0,2,0, lose[i][a]);
-                    lose[i][a] = calcLose(1,1,2,2, lose[i][a]);
-                }else if(i == 0 && a == 1){
-                    lose[i][a] = calcLose(0,0,0,2, lose[i][a]);
-                    lose[i][a] = calcLose(1,1,2,1, lose[i][a]);
-                }else if(i == 0 && a == 2){
-                    lose[i][a] = calcLose(0,0,0,1, lose[i][a]);
-                    lose[i][a] = calcLose(1,2,2,2, lose[i][a]);
-                    lose[i][a] = calcLose(1,1,2,0, lose[i][a]);
-                }else if(i == 1 && a == 0){
-                    lose[i][a] = calcLose(0,0,2,0, lose[i][a]);
-                    lose[i][a] = calcLose(1,1,1,2, lose[i][a]);
-                }else if(i == 1 && a == 1){ // 4
-                    lose[i][a] = calcLose(0,0,2,2, lose[i][a]);
-                    lose[i][a] = calcLose(0,2,2,0, lose[i][a]);
-                    lose[i][a] = calcLose(1,0,1,2, lose[i][a]);
-                    lose[i][a] = calcLose(0,1,2,1, lose[i][a]);
-                }else if(i == 1 && a == 2){
-                    lose[i][a] = calcLose(0,2,2,2, lose[i][a]);
-                    lose[i][a] = calcLose(1,0,1,1, lose[i][a]);
-                }else if(i == 2 && a == 0){
-                    lose[i][a] = calcLose(0,0,1,0, lose[i][a]);
-                    lose[i][a] = calcLose(0,2,1,1, lose[i][a]);
-                    lose[i][a] = calcLose(2,1,2,2, lose[i][a]);
-                }else if(i == 2 && a == 1){
-                    lose[i][a] = calcLose(0,1,1,1, lose[i][a]);
-                    lose[i][a] = calcLose(2,0,2,2, lose[i][a]);
-                }else if(i == 2 && a == 2) {
+                } else if (i == 0 && a == 0) {
+                    lose[i][a] = calcLose(0, 1, 0, 2, lose[i][a]);
+                    lose[i][a] = calcLose(1, 0, 2, 0, lose[i][a]);
+                    lose[i][a] = calcLose(1, 1, 2, 2, lose[i][a]);
+                } else if (i == 0 && a == 1) {
+                    lose[i][a] = calcLose(0, 0, 0, 2, lose[i][a]);
+                    lose[i][a] = calcLose(1, 1, 2, 1, lose[i][a]);
+                } else if (i == 0 && a == 2) {
+                    lose[i][a] = calcLose(0, 0, 0, 1, lose[i][a]);
+                    lose[i][a] = calcLose(1, 2, 2, 2, lose[i][a]);
+                    lose[i][a] = calcLose(1, 1, 2, 0, lose[i][a]);
+                } else if (i == 1 && a == 0) {
+                    lose[i][a] = calcLose(0, 0, 2, 0, lose[i][a]);
+                    lose[i][a] = calcLose(1, 1, 1, 2, lose[i][a]);
+                } else if (i == 1 && a == 1) { // 4
+                    lose[i][a] = calcLose(0, 0, 2, 2, lose[i][a]);
+                    lose[i][a] = calcLose(0, 2, 2, 0, lose[i][a]);
+                    lose[i][a] = calcLose(1, 0, 1, 2, lose[i][a]);
+                    lose[i][a] = calcLose(0, 1, 2, 1, lose[i][a]);
+                } else if (i == 1 && a == 2) {
+                    lose[i][a] = calcLose(0, 2, 2, 2, lose[i][a]);
+                    lose[i][a] = calcLose(1, 0, 1, 1, lose[i][a]);
+                } else if (i == 2 && a == 0) {
+                    lose[i][a] = calcLose(0, 0, 1, 0, lose[i][a]);
+                    lose[i][a] = calcLose(0, 2, 1, 1, lose[i][a]);
+                    lose[i][a] = calcLose(2, 1, 2, 2, lose[i][a]);
+                } else if (i == 2 && a == 1) {
+                    lose[i][a] = calcLose(0, 1, 1, 1, lose[i][a]);
+                    lose[i][a] = calcLose(2, 0, 2, 2, lose[i][a]);
+                } else if (i == 2 && a == 2) {
                     lose[i][a] = calcLose(0, 0, 1, 1, lose[i][a]);
                     lose[i][a] = calcLose(0, 2, 1, 2, lose[i][a]);
                     lose[i][a] = calcLose(2, 0, 2, 1, lose[i][a]);
-                }else {
+                } else {
                     System.out.println("Ivyko klaida AI chance lose");
                 }
             }
@@ -177,31 +179,35 @@ public class TicTacToeAi {
         return lose;
     }
 
-    private static int calcWin(int x, int z, int x2, int z2, int calc_win){
+    private static int calcWin(int x, int z, int x2, int z2, int calc_win) {
         TicTacToeMain ticTacToeMain = new TicTacToeMain();
         String[][] cor = ticTacToeMain.getCor();
         int win = calc_win;
-        if((cor[x][z].equals("X")) && (cor[x2][z2].equals("X"))){ win = 4;  }
-        if (((cor[x2][z2].equals("X"))) && ((cor[x][z].equals(" "))) || ((cor[x][z].equals("X")) && (cor[x2][z2].equals(" ")))){
-            if(win < 1){
+        if ((cor[x][z].equals("X")) && (cor[x2][z2].equals("X"))) {
+            win = 4;
+        }
+        if (((cor[x2][z2].equals("X"))) && ((cor[x][z].equals(" "))) || ((cor[x][z].equals("X")) && (cor[x2][z2].equals(" ")))) {
+            if (win < 1) {
                 win = 1;
             }
         }
         return win;
     }
-    private static int calcLose(int x, int z, int x2, int z2, int calc_lose){
+
+    private static int calcLose(int x, int z, int x2, int z2, int calc_lose) {
         TicTacToeMain ticTacToeMain = new TicTacToeMain();
         String[][] cor = ticTacToeMain.getCor();
-
         int lose = calc_lose;
-        if((cor[x][z].equals("0")) && (cor[x2][z2].equals("0"))){ lose = 4;  }
-        if (((cor[x2][z2].equals("0"))) || ((cor[x][z].equals("0")))){
-            if(lose < 1){
+        if ((cor[x][z].equals("0")) && (cor[x2][z2].equals("0"))) {
+            lose = 4;
+        }
+        if (((cor[x2][z2].equals("0"))) || ((cor[x][z].equals("0")))) {
+            if (lose <= 1) {
                 lose = 1;
             }
         }
-        if((((cor[x2][z2].equals("X"))) && ((cor[x][z].equals("0")))) || (((cor[x2][z2].equals("0"))) || ((cor[x][z].equals("X"))))){
-            if(lose !=2){
+        if ((((cor[x2][z2].equals("X"))) && ((cor[x][z].equals("0")))) || (((cor[x2][z2].equals("0"))) && ((cor[x][z].equals("X"))))) {
+            if (lose != 4) {
                 lose = 0;
             }
         }
@@ -211,6 +217,7 @@ public class TicTacToeAi {
     public int getxAI() {
         return xAI;
     }
+
     public int getyAI() {
         return yAI;
     }
--- src/Object/Object7/Object7.java
+++ src/Object/Object7/Object7.java
@@ -2,7 +2,7 @@ package Object.Object7;
 
 import java.util.Scanner;
 
-public class Object8 {
+public class Object7 {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         int kiek = scanner.nextInt();
